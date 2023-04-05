b73101d8a45fed6a7d38fabd0ae32e52910f58d4
Danila Borisov Vakhov <danilaborisovvakhov@gmail.com>
2023-04-05 23:26:33 +0200

    finished the teacher's service basic functions

--- src/main/java/Service/TeachersService.java
+++ src/main/java/Service/TeachersService.java
@@ -4,12 +4,13 @@ import DTOs.ClassroomsDTO;
 import DTOs.StudentsDTO;
 import DTOs.TeachersDTO;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public interface TeachersService {
     void studentInfo(StudentsDTO student, TeachersDTO teacher) throws SQLException;
     void listOfStudentsInClassroom(ClassroomsDTO classroom);
-    void setGrade(StudentsDTO student, TeachersDTO teacher);
+    void setGrade(StudentsDTO student, TeachersDTO teacher) throws IOException;
     void addStudentToClassroom(StudentsDTO student, ClassroomsDTO classroom);
 
 }
--- src/main/java/Service/TeachersServiceImplementation.java
+++ src/main/java/Service/TeachersServiceImplementation.java
@@ -4,13 +4,19 @@ import Repository.ClassroomsRepository;
 import Repository.Entities.Classrooms;
 import Repository.Entities.StudentsInClassroom;
 import Repository.GradesRepository;
+import Repository.StudentsInClassroomsRepository;
 import Repository.StudentsRepository;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
 import java.util.List;
 
 public class TeachersServiceImplementation implements TeachersService{
     private final ChangeClassService changeClassService = new ChangeClassService();
     private final GradesRepository gradesRepository = new GradesRepository();
+    private final StudentsInClassroomsRepository studentsInClassroomsRepository = new StudentsInClassroomsRepository();
 
     @Override
     public void studentInfo(StudentsDTO student, TeachersDTO teacher) throws SQLException {
@@ -39,25 +45,46 @@ public class TeachersServiceImplementation implements TeachersService{
     @Override
     public void listOfStudentsInClassroom(ClassroomsDTO classroom) {
         List<StudentsInClassroomDTO> students = classroom.getStudents();
-        System.out.println("Classroom: " + classroom.getName());
-        System.out.println("");
+        System.out.println("Classroom: " + classroom.getName() + "\n");
         for(StudentsInClassroomDTO studentsInClassroomDTO : students){
             System.out.println(studentsInClassroomDTO.getStudent().getName());
             System.out.println(studentsInClassroomDTO.getStudent().getDni());
-            System.out.println(studentsInClassroomDTO.getStudent().getId());
-            System.out.println("");
+            System.out.println(studentsInClassroomDTO.getStudent().getId() + "\n");
         }
     }
 
     @Override
-    public void setGrade(StudentsDTO student, TeachersDTO teacher) {
-        GradesDTO grade = new GradesDTO()
-        gradesRepository.setNewGradeInDB();
+    public void setGrade(StudentsDTO student, TeachersDTO teacher) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("The grade it's final? yes(1)/no(any number):");
+        int option = Integer.parseInt(bufferedReader.readLine());
+        boolean itsfinal = option == 1;
+
+        System.out.println("Description: ");
+        String description = bufferedReader.readLine();
+
+        System.out.println("Grade: ");
+        int ngrade = Integer.parseInt(bufferedReader.readLine());
+
+        GradesDTO grade = new GradesDTO(
+                itsfinal,
+                student,
+                teacher.getSubject(),
+                teacher.getSubject().getCourse(),
+                ngrade,
+                description
+        );
+
+        gradesRepository.setNewGradeInDB(changeClassService.changeToGradesEntity(grade));
     }
 
     @Override
     public void addStudentToClassroom(StudentsDTO student, ClassroomsDTO classroom) {
-
+        studentsInClassroomsRepository.setNewStudentInClassroomInDB(
+            changeClassService.changeToStudentEntity(student),
+            changeClassService.changeToClassroomEntity(classroom)
+        );
     }
 
 
